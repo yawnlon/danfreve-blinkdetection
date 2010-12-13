177642007bcc219bcaf8229c0b6fdb12c187544b
frevedb <frevedb@46934dba-dbb4-804b-8a0d-e6c0aaad934e>
2010-12-13 01:49:03 +0000

    Implemented "hello world" project with OpenCV.

---
+++ src/OpenCV_Helloworld/OpenCV_Helloworld/OpenCV_Helloworld.cpp
@@ -0,0 +1,22 @@
+// OpenCV_Helloworld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <cv.h>
+#include <cxcore.h>
+#include <highgui.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    IplImage *img = cvLoadImage("test.bmp");
+    cvNamedWindow("Image:",1);
+    cvShowImage("Image:",img);
+
+    cvWaitKey();
+    cvDestroyWindow("Image:");
+    cvReleaseImage(&img);
+
+    return 0;
+}
+
